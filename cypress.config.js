a7bdf20e41dac0addf67796acab9204692b796de
Ashish Vishwakarma <av.vishwakarma@hotmail.com>
2023-05-02 16:31:45 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,18 +4,18 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
+//       require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: "http://localhost:4100",
     screenshotOnRunFailure: true,
     projectId: "su3jqa",
   },
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportPageTitle: 'Automation Report',
-    inlineAssets: true,
-    saveAllAttempts: false,
-  },
+//   reporter: 'cypress-mochawesome-reporter',
+//   reporterOptions: {
+//     reportPageTitle: 'Automation Report',
+//     inlineAssets: true,
+//     saveAllAttempts: false,
+//   },
   env: {
     // implement environment variables here
     apiUrl: "https://conduit.productionready.io/api",
